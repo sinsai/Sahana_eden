ba0176bbb56997fa34d9423151efb76a1dc1ad66
Fran Boon <flavour@partyvibe.com>
2009-01-21 00:06:49 +0000

    Added Selenium into static along with some initial Tests (Register/Login) Unit Tests started via 2 approaches: SeleniumRC & WebTest (Selenium more promising) Updated code for PyLint recommendations Remove unused files

--- modules/s3_test.py
+++ modules/s3_test.py
@@ -1,42 +1,25 @@
 import wsgi_intercept.webtest_intercept
 
 class WSGI_Test(wsgi_intercept.webtest_intercept.WebCase):
-    "Class to use for Testing Sahana in DocTests"
+    """Class to use for Testing Sahana
+    - DocTests can call this direct
+    - UnitTests can extend this
+    """
     HTTP_CONN = wsgi_intercept.WSGI_HTTPConnection
-    HOST='localhost'
-    PORT=8000
+    HOST = 'localhost'
+    PORT = 8000
     
-    def __init__(self,db):
-        self.db=db
+    def __init__(self, db):
+        "Copy Intialise"
+        wsgi_intercept.webtest_intercept.WebCase.__init__(self)
+        #self.db=db
+        return
 
     def setUp(self):
-        # Not working :/
-        #self.shn_create_testdata(db)
+        "Hook into the WSGI process"
         wsgi_intercept.add_wsgi_intercept(self.HOST, self.PORT, create_fn)
+        return
 
     def runTest(self):
         "Mandatory method for all TestCase instances"
         return
-
-    def shn_create_testdata(self,db):
-        "Create test Data for all modules"
-        self.shn_create_testdata_cr(db)
-        return
-
-    def shn_create_testdata_cr(self,db):
-        "Create test Data for Shelter Registry"
-        module='cr'
-        resource='shelter'
-        table=module+'_'+resource
-        if not len(db().select(db[table].ALL)):
-            db[table].insert(
-                name="Test Shelter",
-            description="Just a test",
-            location_id=1,
-            person_id=1,
-            address='52 Test Street',
-            capacity=100,
-            dwellings=10,
-            persons_per_dwelling=10,
-            area='1 sq km'
-            )
--- modules/sahana.py
+++ modules/sahana.py
@@ -1,6 +1,7 @@
 """
 This file was developed by Fran Boon as a web2py extension.
-This file is released under the BSD license (you can include it in bytecode compiled web2py apps as long as you acknowledge the author). web2py (required to run this file) is released under the GPLv2 license.
+This file is released under the BSD license (you can include it in bytecode compiled web2py apps as long as you acknowledge the author).
+web2py (required to run this file) is released under the GPLv2 license.
 """
 
 from gluon.storage import Storage
@@ -9,7 +10,8 @@ from gluon.http import *
 from gluon.validators import *
 from gluon.sqlhtml import *
 from gluon.contrib.markdown import WIKI
-try: from gluon.contrib.gql import SQLTable
+try:
+    from gluon.contrib.gql import SQLTable
 except ImportError: from gluon.sql import SQLTable
 import traceback
 
@@ -18,7 +20,7 @@ from applications.sahana.modules.t2 import T2
 
 # Modified versions of URL from gluon/html.py
 # we need simplified versions for our jquery functions
-def URL2(a=None,c=None,r=None):
+def URL2(a=None, c=None, r=None):
     """
     example:
 
@@ -35,19 +37,21 @@ def URL2(a=None,c=None,r=None):
     
     The function (& optionally args/vars) are expected to be added via jquery based on attributes of the item.
     """
-    application=controller=None
+    application = controller = None
     if r:
-        application=r.application
-        controller=r.controller
-    if a: application=a    
-    if c: controller=c
+        application = r.application
+        controller = r.controller
+    if a:
+        application = a    
+    if c:
+        controller = c
     if not (application and controller):
         raise SyntaxError, 'not enough information to build the url'
-    other=''
-    url='/%s/%s' % (application, controller)
+    #other = ''
+    url = '/%s/%s' % (application, controller)
     return url
         
-def URL3(a=None,r=None):
+def URL3(a=None, r=None):
     """
     example:
 
@@ -64,31 +68,33 @@ def URL3(a=None,r=None):
     
     The controller & function (& optionally args/vars) are expected to be added via jquery based on attributes of the item.
     """
-    application=controller=None
+    application = controller = None
     if r:
-        application=r.application
-        controller=r.controller
-    if a: application=a    
+        application = r.application
+        controller = r.controller
+    if a:
+        application = a    
     if not (application and controller):
         raise SyntaxError, 'not enough information to build the url'
-    other=''
-    url='/%s' % application
+    #other = ''
+    url = '/%s' % application
     return url
         
-# Extended version of T2 from modules/t2.py 
 class S3(T2):
+    "Extended version of T2 from modules/t2.py"
 
-    def __init__(self,request,response,session,cache,T,db,all_in_db=False):
-        T2.__init__(self,request,response,session,cache,T,db,all_in_db=False)
+    def __init__(self, request, response, session, cache, T, db, all_in_db=False):
+        "Initialise parent class & make any necessary modifications"
+        T2.__init__(self, request, response, session, cache, T, db, all_in_db=False)
         # Clear unused info from cluttering every Response
-        response.files=[]
+        response.files = []
         # Set email server's port?
-        #self.email_server='localhost:25'
+        #self.email_server = 'localhost:25'
     
     # Modified version of _stamp
     # we need to support multiple tables
     # NB Needs testing!
-    def _stamp_many(self,tables,form,create=False):
+    def _stamp_many(self, tables, form, create=False):
         """
         Called by create and update methods. it timestamps the records.
         The following fields are timestamped (if they exist):
@@ -103,63 +109,63 @@ class S3(T2):
         for table in tables:
             if create:
                 if 'created_by_ip' in table.fields: 
-                    form.vars.created_by_ip=self.request.client
+                    form.vars.created_by_ip = self.request.client
                 if 'created_on' in table.fields: 
-                    form.vars.created_on=self.now
+                    form.vars.created_on = self.now
                 if 'created_by' in table.fields: 
-                    form.vars.created_by=self.person_id
+                    form.vars.created_by = self.person_id
                 if 'created_signature' in table.fields: 
-                    form.vars.created_signature=self.person_name
+                    form.vars.created_signature = self.person_name
             if 'modified_by_ip' in table.fields:
-                form.vars.modified_by_ip=self.request.client
+                form.vars.modified_by_ip = self.request.client
             if 'modified_on' in table.fields: 
-                form.vars.modified_on=self.now
+                form.vars.modified_on = self.now
             if 'modified_by' in table.fields:
-                form.vars.modified_by=self.person_id
+                form.vars.modified_by = self.person_id
             if 'modified_signature' in table.fields:
-                form.vars.modified_signature=self.person_name
+                form.vars.modified_signature = self.person_name
 
-    def register(self,verification=False,sender='',next='login',onaccept=None):
+    def register(self, verification=False, sender='', next='login', onaccept=None):
         """
         Overrides t2's register() to add new functionality:
             * Whenever someone registers, it automatically adds their name to the Person Registry
-            * Registering automaticallu logs you in
+            * Registering automatically logs you in
         
         To use, create a controller:
 
         def register(): return t2.register()
         """
-        request,response,session,cache,T,db=self._globals()
+        request, response, session, cache, T, db = self._globals()
         def onaccept2(form):
-            db.t2_membership.insert(person_id=form.vars.id,group_id=db.t2_group.insert(name=form.vars.name),status='approved')
+            db.t2_membership.insert(person_id=form.vars.id, group_id=db.t2_group.insert(name=form.vars.name), status='approved')
             # S3: Add to Person Registry as well
             # Check to see whether User already exists
             if len(db(db.pr_person.email==form.vars.email).select()):
                 # Update
                 #db(db.pr_person.email==form.vars.email).select()[0].update_record(
-                #    name=form.vars.name
+                #    name = form.vars.name
                 #)
                 pass
             else:
                 # Insert
                 db.pr_person.insert(
-                    name=form.vars.name,
-                    email=form.vars.email
+                    name = form.vars.name,
+                    email = form.vars.email
                 )
             # S3: Login automatically upon registration
             if not verification:
-                session.t2.person_id=form.vars.id
-                session.t2.person_name=form.vars.name
-                session.t2.person_email=form.vars.email
-                #session.confirmation=self.messages.logged_in
+                session.t2.person_id = form.vars.id
+                session.t2.person_name = form.vars.name
+                session.t2.person_email = form.vars.email
+                #session.confirmation = self.messages.logged_in
 
             if form.vars.registration_key:
-                body=self.messages.register_email_body % dict(form.vars)
-                if not self.email(sender=sender,to=form.vars.email,subject=self.messages.register_email_subject,message=body):
+                body = self.messages.register_email_body % dict(form.vars)
+                if not self.email(sender=sender, to=form.vars.email, subject=self.messages.register_email_subject, message=body):
                     self.redirect(flash=self.messages.unable_to_send_email)
-                session.flash=self.messages.email_sent
+                session.flash = self.messages.email_sent
             if onaccept:
                 onaccept(form)
-        vars={'registration_key':
+        vars = {'registration_key':
             str(uuid.uuid4()) if verification else ''}
-        return self.create(self.db.t2_person,vars=vars,onaccept=onaccept2,next=next)
+        return self.create(self.db.t2_person, vars=vars, onaccept=onaccept2, next=next)

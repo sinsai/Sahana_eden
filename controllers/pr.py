3e8b4fbbe18c5f1c6cad12fcb84624cd7176d8c1
Dominic König <dominic@polaris>
2009-07-16 18:52:26 +0000

    Added VITA Added Identities to PR

--- controllers/pr.py
+++ controllers/pr.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 module = 'pr'
 # Current Module (for sidebar title)
@@ -6,9 +6,15 @@ module_name = db(db.s3_module.name==module).select()[0].name_nice
 # Options Menu (available in all Functions' Views)
 response.menu_options = [
     [T('Home'), False, URL(r=request, f='index')],
-    [T('Add Person'), False, URL(r=request, f='person', args='create')],
-    [T('List People'), False, URL(r=request, f='person')],
-    [T('Search People'), False, URL(r=request, f='person', args='search')]
+    [T('Persons'), False, '#',[
+        [T('Add Person'), False, URL(r=request, f='person', args='create')],
+        [T('List People'), False, URL(r=request, f='person')],
+        [T('Search People'), False, URL(r=request, f='person', args='search')]
+    ]],
+    [T('Identities'), False, '#',[
+        [T('Add Identity'), False, URL(r=request, f='identity', args='create')],
+        [T('List Identites'), False, URL(r=request, f='identity')]
+    ]]
 ]
 
 # S3 framework functions
@@ -19,6 +25,9 @@ def index():
 def person():
     "RESTlike CRUD controller"
     return shn_rest_controller(module, 'person', main='first_name', extra='last_name')
+def identity():
+    "RESTlike CRUD controller"
+    return shn_rest_controller(module, 'identity')
 def contact():
     "RESTlike CRUD controller"
     return shn_rest_controller(module, 'contact')
---
+++ models/04_vita.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#
+# Person Tracking and Tracing (VITA)
+#
+# created 2009-07-14 by nursix
+#
+
+module = 'vita'
+
+# Settings
+resource = 'setting'
+table = module + '_' + resource
+db.define_table(table,
+                Field('audit_read', 'boolean'),
+                Field('audit_write', 'boolean'),
+                migrate=migrate)
+# Populate table with Default options
+# - deployments can change these live via appadmin
+if not len(db().select(db[table].ALL)): 
+   db[table].insert(
+        # If Disabled at the Global Level then can still Enable just for this Module here
+        audit_read = False,
+        audit_write = False
+    )
+

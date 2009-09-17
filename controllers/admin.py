0b2021f436c237f2ee7cd270226386881a57232a
Fran Boon <flavour@partyvibe.com>
2009-09-17 07:39:54 +0100

    Themes: Check for readability of all necessary areas

--- controllers/admin.py
+++ controllers/admin.py
@@ -32,13 +32,21 @@ def theme_apply(form):
         col_highlight = theme.col_highlight
         
         template = os.path.join(request.folder, 'static', 'styles', 'S3', 'template.css')
+        tmp_folder = os.path.join(request.folder, 'static', 'scripts', 'tools')
         out_file = os.path.join(request.folder, 'static', 'styles', 'S3', 'sahana.css')
+        out_file2 = os.path.join(request.folder, 'static', 'styles', 'S3', 'sahana.min.css')
         # Check permissions
         if not os.access(template, os.R_OK):
             session.error = T('Template file %s not readable - unable to apply theme!' % template)
             redirect(URL(r=request, args=request.args))
+        if not os.access(tmp_folder, os.W_OK):
+            session.error = T('Temp folder %s not writable - unable to apply theme!' % tmp_folder)
+            redirect(URL(r=request, args=request.args))
         if not os.access(out_file, os.W_OK):
             session.error = T('CSS file %s not writable - unable to apply theme!' % out_file)
+            redirect(URL(r=request, args=request.args))
+        if not os.access(out_file2, os.W_OK):
+            session.error = T('CSS file %s not writable - unable to apply theme!' % out_file2)
             redirect(URL(r=request, args=request.args))
         # Read in Template
         inpfile = open(template, 'r')

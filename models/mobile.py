40324a0b02958dc0b0e60bcc2549ea3c9257ce74
Fran Boon <flavour@partyvibe.com>
2009-10-15 12:44:32 +0100

    merge lifeeth: thickbox popups fix & mobile

---
+++ models/mobile.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+
+module = 'mobile'
+
+# Settings
+resource = 'settings'
+table = module + '_' + resource
+db.define_table(table,
+                Field('port'),                            # Port for the modem
+                Field('baud','integer', default = 115200),          # Modem Baud rate
+                migrate=migrate)
+if not len(db().select(db[table].ALL)):
+   db[table].insert(baud=115200)

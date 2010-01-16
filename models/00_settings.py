77d95662c161597a21e46dd0b37596f6e28e7c74
Fran Boon <flavour@partyvibe.com>
2010-01-16 12:20:59 +0000

    Merge nusix: GeoRSS, KML fix

--- models/00_settings.py
+++ models/00_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-S3_PUBLIC_URL = 'http://127.0.0.1:8000/sahana'
+S3_PUBLIC_URL = 'http://127.0.0.1:8000'
 S3_UTC_OFFSET = 'UTC +0000' # default UTC offset (timezone) of users
 BREADCRUMB = '>> '
 

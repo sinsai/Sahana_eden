63d4c48566a9ecaf878f0f8261623fd9f984df7d
Fran Boon <flavour@partyvibe.com>
2010-01-16 23:15:32 +0000

    Advanced JavaScript Layers Move Coords to Top-Right so they don't interfere with Terms of Use

--- views/gis/ol_layers_all.js
+++ views/gis/ol_layers_all.js
@@ -26,4 +26,5 @@
   {{include 'gis/ol_layers_georss.js'}}
   {{include 'gis/ol_layers_gpx.js'}}
   {{include 'gis/ol_layers_kml.js'}}
+  {{include 'gis/ol_layers_js.js'}}
 }
---
+++ views/gis/ol_layers_js.js
@@ -0,0 +1,3 @@
+﻿{{for layer in js_layers:}}
+    {{=js_layers[layer].code}}
+{{pass}}

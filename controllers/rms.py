99c47f3ac64fe889c6c97623b78d55c629a4211c
Matt Terry <mrterry@purple-box>
2010-01-18 20:19:54 -0600

    * Modified request/pledge items forms for nicer output

--- controllers/rms.py
+++ controllers/rms.py
@@ -19,7 +19,12 @@ response.menu_options = [
 # S3 framework functions
 def index():
     "Module's Home Page"
-    return dict(module_name=module_name)
+    return dict(module_name=module_name, a=1)
+
+def test():
+    "Module's Home Page"
+    return dict(module_name=module_name, a=1)
+
 
 @service.jsonrpc
 @service.xmlrpc
@@ -41,10 +46,8 @@ def pledge_aid():
 def sms_request():
     "RESTlike CRUD controller"
     return shn_rest_controller(module, 'sms_request', editable=False, listadd=False)
-    
-    
-def shn_rms_req_pheader(resource, record_id, representation, next=None, same=None):
 
+def shn_rms_req_pheader(resource, record_id, representation, next=None, same=None):
     if representation == "html":
 
         if next:
@@ -59,23 +62,46 @@ def shn_rms_req_pheader(resource, record_id, representation, next=None, same=Non
 
         request_aid = db(db.rms_request_aid.id == record_id).select().first()
 
-        pheader = TABLE(
-            TR(
+        top_row = [
                 TH(T('Request Aid: ')),
-                request_aid.id,
+                A(T(str(request_aid.id)),
+                    _href=URL(r=request, f=resource, args=request_aid.id)),
+
                 TH(T('Priority: ')),
-                request_aid.priority,
+                rms_priority_opts[request_aid.priority],
                 TH(T('Number Served: ')),
                 request_aid.numserved,
-                ),
-            TR(
-                TH(T('Organization: ')),
-                request_aid.organisation_id,
-                TH(T('Person: ')),
-                request_aid.person_id,
-                TH(T('Location: ')),
-                request_aid.location_id,
-            ),
+                ]
+
+        bottom_row = []
+
+        person = db(db.pr_person.id==request_aid.person_id).select()
+        bottom_row += [ TH(T('Person: ')) ]
+        if len(person) > 0:
+            person = person[0]
+            bottom_row += [ A(T(person.first_name + " " + person.last_name), _href=URL(a='rms', c='pr', f='person', args=person.id)) ]
+        else:
+            bottom_row += ['']
+
+        org = db(db.or_organisation.id==request_aid.organisation_id).select()
+        bottom_row += [ TH(T('Organization: ')) ]
+        if len(org) > 0:
+            org = org[0]
+            bottom_row += [ A(T(org.name), _href=URL(a='rms', c='or', f='organisation', args=org.id)) ]
+        else:
+            bottom_row += ['None']
+
+        loc = db(db.gis_location.id==request_aid.location_id).select()
+        bottom_row += [ TH(T('Location: ')) ]
+        if len(loc) > 0:
+            loc = loc[0]
+            bottom_row += [ A(T(loc.name), _href=URL(a='rms', c='gis', f='location', args=loc.id)) ]
+        else:
+            bottom_row += ['None']
+
+        pheader = TABLE(
+            TR(top_row),
+            TR( bottom_row),
         )
         return pheader
 
@@ -84,7 +110,6 @@ def shn_rms_req_pheader(resource, record_id, representation, next=None, same=Non
         
         
 def shn_rms_plg_pheader(resource, record_id, representation, next=None, same=None):
-
     if representation == "html":
 
         if next:
@@ -99,23 +124,48 @@ def shn_rms_plg_pheader(resource, record_id, representation, next=None, same=Non
 
         pledge_aid = db(db.rms_pledge_aid.id == record_id).select().first()
 
-        pheader = TABLE(
-            TR(
+        top_row = [
                 TH(T('Pledge Aid: ')),
-                pledge_aid.id,
+                A(T(str(pledge_aid.id)),
+                    _href=URL(r=request, f=resource, args=pledge_aid.id)),
                 TH(T('Priority: ')),
-                pledge_aid.priority,
-                ),
-            TR(
-                TH(T('Organization: ')),
-                pledge_aid.organisation_id,
-                TH(T('Person: ')),
-                pledge_aid.person_id,
-                TH(T('Location: ')),
-                pledge_aid.location_id,
-            ),
+                rms_priority_opts[pledge_aid.priority],
+                TH(T('')),
+                ''
+                ]
+
+        bottom_row = []
+
+        person = db(db.pr_person.id==pledge_aid.person_id).select()
+        bottom_row += [ TH(T('Person: ')) ]
+        if len(person) > 0:
+            person = person[0]
+            bottom_row += [ A(T(person.first_name + " " + person.last_name), _href=URL(a='rms', c='pr', f='person', args=person.id)) ]
+        else:
+            bottom_row += ['']
+
+        org = db(db.or_organisation.id==pledge_aid.organisation_id).select()
+        bottom_row += [ TH(T('Organization: ')) ]
+        if len(org) > 0:
+            org = org[0]
+            bottom_row += [ A(T(org.name), _href=URL(a='rms', c='or', f='organisation', args=org.id)) ]
+        else:
+            bottom_row += ['None']
+
+        loc = db(db.gis_location.id==pledge_aid.location_id).select()
+        bottom_row += [ TH(T('Location: ')) ]
+        if len(loc) > 0:
+            loc = loc[0]
+            bottom_row += [ A(T(loc.name), _href=URL(a='rms', c='gis', f='location', args=loc.id)) ]
+        else:
+            bottom_row += ['None']
+
+        pheader = TABLE(
+            TR(top_row),
+            TR( bottom_row),
         )
         return pheader
 
     else:
         return None
+

c5182db79238f3ebbf408492badcf93d493620a0
Milad Fatenejad <fateneja@animal2>
2010-01-20 01:56:23 -0600

    Prettified SMS mapping info using media_metadata

--- cron/rms_entry2record.py
+++ cron/rms_entry2record.py
@@ -9,6 +9,7 @@ feature_id = feature[0]['id'] if len(feature) == 1 else None
 def rss2record(entry):
     myd = {}
     locd = {}
+
     myd['ush_id'] = entry['id']
     myd['link'] = entry['link']	 # url for the entry
     myd['author'] = entry['author']
@@ -31,13 +32,23 @@ def rss2record(entry):
     myd['department'] = entry['department']
     myd['summary'] = entry['summary']
     myd['notes'] = entry['notes']
+    
+    # Fix escape characters: 
+    myd["notes"] = " ".join(myd["notes"].split())
+    myd["notes"] = myd["notes"].replace('\\"', '"')
+    myd["notes"] = myd["notes"].replace("\\'", "'")
 
+    # Add location information. The key name contains a colon, and for
+    # this reason, is platform dependent. Thus, we just look for any
+    # entry that ends in "point":
     for key in entry.keys():
         if key[-5:] == "point":
+            # Found the location info
             gpoint = entry[key].split()
             lat = float(gpoint[0])
             lon = float(gpoint[1])
 
+            # Ushahidi uses a 0,0 lat/lon to indicate no lat lon.
             if abs(lat) > 1.0e-8 and abs(lon) > 1.0e-8:
                 locd['lat' ] = lat
                 locd['lon' ] = lon
@@ -48,18 +59,22 @@ def rss2record(entry):
                     name += "No category"
 
                 locd['name'] = name
-				
+
                 if marker_id is not None:
                     locd['marker_id'] = marker_id
                 if feature_class_id is not None:
                     locd['feature_class_id'] = feature_id
+
+
     return myd, locd
 
 import datetime
 import gluon.contrib.feedparser as feedparser
 url_base = "http://75.101.195.137/rss.php?key=yqNm7FHSwfdRb8nC2653"
 
-N = 50
+ids = []
+
+N = 100
 start = 0
 done = False
 while done == False:
@@ -79,14 +94,46 @@ while done == False:
                 locid = db.gis_location.insert(**locd)
 
             rec["location_id"] = locid
-            db.rms_sms_request.insert(**rec)
+            smsid = db.rms_sms_request.insert(**rec)
+            if locid != None:
+                ids.append((smsid,locid))
+
+            # Add media_metadata entry to show additional
+            # information on the map
+            metadata = {}
+            metadata["event_time" ] = rec["updated"]
+
+            desc = rec["sms"]
+
+            # If there is a note, append it to the description:
+            if rec["notes"] != "":
+                # New lines, apstrophes, and quotes break the mapping.
+                rec["notes"] = " ".join(rec["notes"].split())
+                rec["notes"] = rec["notes"].replace('"', '\\"')
+                rec["notes"] = rec["notes"].replace("'", "\\'")
+
+                #rec["notes"] = rec["notes"].replace('\\', '\\\\')
+                #rec["notes"] = rec["notes"].replace('\\\\"', '\\"')
+                #rec["notes"] = rec["notes"].replace("\\\\'", "\\'")
+
+                desc = rec["sms"] + " NOTE: " + rec["notes"]
+
+            metadata["description"] = desc
+            metadata["location_id"] = locid
+            db.media_metadata.insert(**metadata)
         else:
             done = True
             break
 
     start = start + N
-
+    
     if len(d["entries"]) == 0:
         done = True
+
+db.commit()
+
+# Now loop through and set the location names with the sms id numbers:
+for smsid, locid in ids:
+    db(db.gis_location.id == locid).update(name="SMS " + repr(smsid))
 
 db.commit()

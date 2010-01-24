c9a796382f3646b61342498ebd5ce79fdfe2b252
Matt Brown <matt@mattb.net.nz>
2010-01-24 18:50:40 +0000

    Basic logic for the cron job to handle import jobs in the 'processing' state.

--- cron/import_job_do_processing.py
+++ cron/import_job_do_processing.py
@@ -7,7 +7,36 @@
 
 jobs = db(db.admin_input_job.status == 'processing').select()
 for job in jobs:
+    # Get job column map.
+    try:
+        column_map = pickle.loads(job.column_map)
+    except pickle.UnpicklingError:
+        column_map = []
+    
+    # Open the input file.
+    filepath = os.path.join(request.folder, 'uploads', filename)
+    reader = csv.reader(open(filepath, 'r'))
+    
+    # Retrieve column headings from the first line.
+    csv_headings = []
+    for line in reader:
+        for col in line:
+            csv_headings.append(col)
+        break
+    if csv_headings != [t[0] for t in column_map]:
+        print 'Cannot process job #%d. Column headings do not match DB!' % job.id
+        continue
+
     # Read each line
+    for line_num, line in enumerate(reader, 0):
+        line_data = {}
+        for idx, col in enumerate(line):
+            field = column_map[idx][1]
+            if not field:
+                continue
+            line_data[field] = col
+        print line_data
+
     # Map CSV headers to model fields
     # validate via SQLFORM
     # store field, value pairs and validity status into an ImportLine model.

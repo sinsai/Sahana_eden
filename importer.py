8ce3332b794db8c53b8e8c6790b6464df08ad3d2
Shikhar Kohli <shikharkohli@gmail.com>
2010-06-26 00:38:39 +0530

    Multiselect for Ext files added, UI is working now

--- importer.py
+++
@@ -1,5 +0,0 @@
-module='importer'
-resource='slist'
-table=module+'_'+resource
-db.define_table(table,timestamp, uuidstamp,Field('Name',length=128,notnull=True),
-            Field('Path',type='upload',uploadfield=True))

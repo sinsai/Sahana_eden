34a4b8c2eaa9f041d2fb1b60b861c9ce8fb12272
Fran Boon <flavour@partyvibe.com>
2010-07-14 16:49:25 +0100

    JS Selector optimisations

--- views/gis/unused/feature_create_unused.js
+++ views/gis/unused/feature_create_unused.js
@@ -1,14 +1,14 @@
 ﻿<script type="text/javascript">//<![CDATA[
 // Unused. We do server-side since easier for the WKTCentroid & anyway we don't want to rely on JS clients
 $(function() {
-    $('input#submit_button').click(function(event){
-        type=$("select#gis_feature_type").val();
+    $('#submit_button').click(function(event){
+        type=$("#gis_feature_type").val();
         if (type=="point") {
             // Populate WKT field from Lat/Lon
-            lat=$("input#gis_feature_lat").val();
-            lon=$("input#gis_feature_lon").val();
+            lat=$("#gis_feature_lat").val();
+            lon=$("#gis_feature_lon").val();
             wkt='POINT('+lon+' '+lat+')'
-            $('input#gis_feature_wkt').val(wkt);
+            $('#gis_feature_wkt').val(wkt);
         } else if (type=="line" | type=="polygon") {
             // Need to port Geos' getCentroid routines from C to JavaScript
             // same as: http://www.jennessent.com/arcgis/shapes_poster.htm

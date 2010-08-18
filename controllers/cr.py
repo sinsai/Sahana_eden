57c7f1584efe12721cea9e18c636bf96671bb3fe
Fran Boon <fran@aidiq.com>
2010-08-18 01:48:54 +0100

    CR supports Types & Services & links to RMS

--- controllers/cr.py
+++ controllers/cr.py
@@ -12,43 +12,129 @@ if module not in deployment_settings.modules:
 
 # Options Menu (available in all Functions' Views)
 response.menu_options = [
-    [T('Add Shelter'), False, URL(r=request, f='shelter', args='create')],
-    [T('List Shelters'), False, URL(r=request, f='shelter')],
-    #[T('Search Shelters'), False, URL(r=request, f='shelter', args='search')]
+    [T("Add Shelter"), False, URL(r=request, f="shelter", args="create")],
+    [T("List Shelters"), False, URL(r=request, f="shelter"), [
+        [T("Services"), False, URL(r=request, f="shelter_service")],
+        [T("Types"), False, URL(r=request, f="shelter_type")],
+    ]],
+    #[T("Search Shelters"), False, URL(r=request, f="shelter", args="search")]
 ]
 
 # S3 framework functions
 def index():
-    "Module's Home Page"
+
+    """ Module's Home Page """
     
     module_name = deployment_settings.modules[module].name_nice
     
     return dict(module_name=module_name)
 
+# -----------------------------------------------------------------------------
+def shelter_type():
+
+    """ RESTful CRUD controller """
+
+    resource = request.function
+
+    # Post-processor
+    def user_postp(jr, output):
+        shn_action_buttons(jr, deletable=False)
+        return output
+    response.s3.postp = user_postp
+
+    output = shn_rest_controller(module, resource,
+                                 rheader=lambda r: \
+                                         shn_shelter_rheader(r,
+                                            tabs = [(T("Basic Details"), None),
+                                                    (T("Shelters"), "shelter")]),
+                                                    sticky=True)
+    return output
+
+# -----------------------------------------------------------------------------
+def shelter_service():
+
+    """ RESTful CRUD controller """
+
+    resource = request.function
+
+    # Post-processor
+    def user_postp(jr, output):
+        shn_action_buttons(jr, deletable=False)
+        return output
+    response.s3.postp = user_postp
+
+    output = shn_rest_controller(module, resource,
+                                 rheader=lambda r: \
+                                         shn_shelter_rheader(r,
+                                            tabs = [(T("Basic Details"), None),
+                                                    (T("Shelters"), "shelter")]),
+                                                    sticky=True)
+    return output
+
+# -----------------------------------------------------------------------------
 def shelter():
+
     """ RESTful CRUD controller
-    >>> resource='shelter'
+
+    >>> resource="shelter"
     >>> from applications.sahana.modules.s3_test import WSGI_Test
     >>> test=WSGI_Test(db)
-    >>> '200 OK' in test.getPage('/sahana/%s/%s' % (module,resource))
+    >>> "200 OK" in test.getPage("/sahana/%s/%s" % (module,resource))
     True
     >>> test.assertHeader("Content-Type", "text/html")
-    >>> test.assertInBody('List Shelters')
-    >>> '200 OK' in test.getPage('/sahana/%s/%s/create' % (module,resource))    #doctest: +SKIP
+    >>> test.assertInBody("List Shelters")
+    >>> "200 OK" in test.getPage("/sahana/%s/%s/create" % (module,resource))    #doctest: +SKIP
     True
     >>> test.assertHeader("Content-Type", "text/html")                          #doctest: +SKIP
-    >>> test.assertInBody('Add Shelter')                                        #doctest: +SKIP
-    >>> '200 OK' in test.getPage('/sahana/%s/%s?format=json' % (module,resource))
+    >>> test.assertInBody("Add Shelter")                                        #doctest: +SKIP
+    >>> "200 OK" in test.getPage("/sahana/%s/%s?format=json" % (module,resource))
     True
     >>> test.assertHeader("Content-Type", "text/html")
-    >>> test.assertInBody('[')
-    >>> '200 OK' in test.getPage('/sahana/%s/%s?format=csv' % (module,resource))
+    >>> test.assertInBody("[")
+    >>> "200 OK" in test.getPage("/sahana/%s/%s?format=csv" % (module,resource))
     True
     >>> test.assertHeader("Content-Type", "text/csv")
-    """
-    resource = request.function
-    return shn_rest_controller(module, resource)
 
+    """
+
+    resource = request.function
+
+    response.s3.pagination = True
+
+    # Post-processor
+    def user_postp(jr, output):
+        shn_action_buttons(jr, deletable=False)
+        return output
+    response.s3.postp = user_postp
+
+    output = shn_rest_controller(module, resource)
+    return output
+
+# -----------------------------------------------------------------------------
+def shn_shelter_rheader(r, tabs=[]):
+
+    """ Resource Headers """
+
+    if r.representation == "html":
+        rheader_tabs = shn_rheader_tabs(r, tabs)
+
+        record = r.record
+        rheader = DIV(TABLE(
+                        TR(
+                            TH(Tstr("Name") + ": "), record.name
+                          ),
+                        ),
+                      rheader_tabs)
+
+        return rheader
+
+    else:
+        return None
+
+
+    
+    
+# -----------------------------------------------------------------------------
 # http://groups.google.com/group/web2py/browse_thread/thread/53086d5f89ac3ae2
 def call():
     "Call an XMLRPC, JSONRPC or RSS service"
@@ -58,18 +144,18 @@ def call():
 @service.xmlrpc
 @service.amfrpc
 def rpc(method,id=0):
-    if method == 'list':
+    if method == "list":
         return db().select(db.cr_shelter.ALL).as_list()
-    if method == 'read':
+    if method == "read":
         return db(db.cr_shelter.id==id).select().as_list()
-    if method == 'delete':
+    if method == "delete":
         status=db(db.cr_shelter.id==id).delete()
         if status:
-            return 'Success - record %d deleted!' % id
+            return "Success - record %d deleted!" % id
         else:
-            return 'Failed - no record %d!' % id
+            return "Failed - no record %d!" % id
     else:
-        return 'Method not implemented!'
+        return "Method not implemented!"
 
 @service.xmlrpc
 def create(name):
@@ -82,6 +168,6 @@ def update(id, name):
     # Need to do validation manually!
     status = db(db.cr_shelter.id == id).update(name=name)
     if status:
-        return 'Success - record %d updated!' % id
+        return "Success - record %d updated!" % id
     else:
-        return 'Failed - no record %d!' % id
+        return "Failed - no record %d!" % id
--- models/cr.py
+++ models/cr.py
@@ -15,19 +15,102 @@ if deployment_settings.has_module(module):
                     Field("audit_write", "boolean"),
                     migrate=migrate)
 
+    # -----------------------------------------------------------------------------
     # Shelters
+    resource = "shelter_type"
+    tablename = module + "_" + resource
+    table = db.define_table(tablename, timestamp, uuidstamp, deletion_status,
+                    Field("name", notnull=True),
+                    comments,
+                    migrate=migrate)
+    
+    ADD_SHELTER_TYPE = T("Add Shelter Type")
+    LIST_SHELTER_TYPES = T("List Shelter Types")
+    s3.crud_strings[tablename] = Storage(
+        title_create = T("Add Shelter Type"),
+        title_display = ADD_SHELTER_TYPE,
+        title_list = LIST_SHELTER_TYPES,
+        title_update = T("Edit Shelter Type"),
+        title_search = T("Search Shelter Types"),
+        subtitle_create = T("Add New Shelter Type"),
+        subtitle_list = T("Shelter Types"),
+        label_list_button = LIST_SHELTER_TYPES,
+        label_create_button = ADD_SHELTER_TYPE,
+        msg_record_created = T("Shelter Type added"),
+        msg_record_modified = T("Shelter Type updated"),
+        msg_record_deleted = T("Shelter Type deleted"),
+        msg_list_empty = T("No Shelter Types currently registered"))
+
+    shelter_type_id = db.Table(None, "shelter_type_id",
+                               Field("shelter_type_id", db.cr_shelter_type,
+                                     requires = IS_NULL_OR(IS_ONE_OF(db, "cr_shelter_type.id", "%(name)s")),
+                                     represent = lambda id: (id and [db.cr_shelter_type[id].name] or ["None"])[0],
+                                     comment = A(ADD_SHELTER_TYPE, _class="colorbox", _href=URL(r=request, c="cr", f="shelter_type", args="create", vars=dict(format="popup")), _target="top", _title=ADD_SHELTER_TYPE),
+                                     ondelete = "RESTRICT",
+                                     label = T("Shelter Type")
+                                    )
+                              )
+
+    # -----------------------------------------------------------------------------
+    resource = "shelter_service"
+    tablename = module + "_" + resource
+    table = db.define_table(tablename, timestamp, uuidstamp, deletion_status,
+                    Field("name", notnull=True),
+                    comments,
+                    migrate=migrate)
+    
+    ADD_SHELTER_SERVICE = T("Add Shelter Service")
+    LIST_SHELTER_SERVICES = T("List Shelter Services")
+    s3.crud_strings[tablename] = Storage(
+        title_create = T("Add Shelter Service"),
+        title_display = ADD_SHELTER_SERVICE,
+        title_list = LIST_SHELTER_SERVICES,
+        title_update = T("Edit Shelter Service"),
+        title_search = T("Search Shelter Services"),
+        subtitle_create = T("Add New Shelter Service"),
+        subtitle_list = T("Shelter Services"),
+        label_list_button = LIST_SHELTER_SERVICES,
+        label_create_button = ADD_SHELTER_SERVICE,
+        msg_record_created = T("Shelter Service added"),
+        msg_record_modified = T("Shelter Service updated"),
+        msg_record_deleted = T("Shelter Service deleted"),
+        msg_list_empty = T("No Shelter Services currently registered"))
+
+    def shn_shelter_service_represent(shelter_service_ids):
+        if not shelter_service_ids:
+            return "None"
+        elif "|" in str(shelter_service_ids):
+            shelter_services = [db(db.cr_shelter_service.id == id).select(db.cr_shelter_service.name, limitby=(0, 1)).first().name for id in shelter_service_ids.split("|") if id]
+            return ", ".join(shelter_services)
+        else:
+            return db(db.cr_shelter_service.id == shelter_service_ids).select(db.cr_shelter_service.name, limitby=(0, 1)).first().name
+
+    shelter_service_id = db.Table(None, "shelter_service_id",
+                                  FieldS3("shelter_service_id", 
+                                          requires = IS_NULL_OR(IS_ONE_OF(db, "cr_shelter_service.id", "%(name)s", multiple=True)),
+                                          represent = shn_shelter_service_represent,
+                                          comment = A(ADD_SHELTER_SERVICE, _class="colorbox", _href=URL(r=request, c="cr", f="shelter_service", args="create", vars=dict(format="popup")), _target="top", _title=ADD_SHELTER_SERVICE),
+                                          ondelete = "RESTRICT",
+                                          label = T("Shelter Service")
+                                         )
+                                 )
+
+    # -----------------------------------------------------------------------------
     resource = "shelter"
     tablename = module + "_" + resource
     table = db.define_table(tablename, timestamp, uuidstamp, deletion_status,
                     Field("name", notnull=True),
+                    shelter_type_id,
                     Field("description"),
                     location_id,
+                    shelter_service_id,
                     person_id,
                     Field("address", "text"),
                     Field("capacity", "integer"),
                     Field("dwellings", "integer"),
                     Field("persons_per_dwelling", "integer"),
                     Field("area"),
+                    comments,
                     migrate=migrate)
     table.uuid.requires = IS_NOT_IN_DB(db, "%s.uuid" % tablename)
     table.name.requires = IS_NOT_EMPTY()   # Shelters don't have to have unique names
@@ -43,6 +126,7 @@ if deployment_settings.has_module(module):
     table.persons_per_dwelling.requires = IS_NULL_OR(IS_INT_IN_RANGE(0, 999))
     table.persons_per_dwelling.label = T("Persons per Dwelling")
     table.area.label = T("Area")
+
     ADD_SHELTER = T("Add Shelter")
     LIST_SHELTERS = T("List Shelters")
     s3.crud_strings[tablename] = Storage(
@@ -60,11 +144,20 @@ if deployment_settings.has_module(module):
         msg_record_deleted = T("Shelter deleted"),
         msg_list_empty = T("No Shelters currently registered"))
 
+    # reusable field
     shelter_id = db.Table(None, "shelter_id",
-                        Field("shelter_id", db.cr_shelter,
+                          Field("shelter_id", db.cr_shelter,
                                 requires = IS_NULL_OR(IS_ONE_OF(db, "cr_shelter.id", "%(name)s")),
                                 represent = lambda id: (id and [db.cr_shelter[id].name] or ["None"])[0],
                                 ondelete = "RESTRICT",
+                                comment = DIV(A(ADD_SHELTER, _class="colorbox", _href=URL(r=request, c="cr", f="shelte", args="create", vars=dict(format="popup")), _target="top", _title=ADD_SHELTER),
+                                          DIV( _class="tooltip", _title=Tstr("Shelter") + "|" + Tstr("The Shelter this Request is from (optional)."))),
                                 label = T("Shelter")
-                            )
-                        )
+                               )
+                         )
+    # Shelters as component of Services, Types & Locations
+    s3xrc.model.add_component(module, resource,
+                              multiple=True,
+                              joinby=dict(cr_shelter_type="shelter_type_id", cr_shelter_service="shelter_service_id", gis_location="location_id"),
+                              deletable=True,
+                              editable=True)

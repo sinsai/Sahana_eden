0e73d1c4047c7325185af240c1c194c577b96972
Michael <Michael@Michael-PC>
2010-08-20 01:27:23 +0700

    document_id added to rms_req

--- controllers/doc.py
+++ controllers/doc.py
@@ -79,7 +79,8 @@ def document():
                                                   (T("Incident Report"), "ireport"),  
                                                   (T("Inventory"), "location"),  
                                                   (T("Shelter"), "shelter"),      
-                                                  (T("Flood Report"), "freport"),                                                                                                 
+                                                  (T("Flood Report"), "freport"),    
+                                                  (T("Request"), "req"),                                                                                               
                                                  ]
                                           )
     

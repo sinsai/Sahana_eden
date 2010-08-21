6f55515edd6478eb07b8a6efdf58e1495563ed17
nursix.org <dominic@nursix.org>
2010-08-21 19:36:43 +0000

    RAT:   - some progress   - code simplification
    CRUD/create:
      - ?from_record=<id> to use a previous record as template
        for a new one, copying all values in writable fields
        => seems to work right away, but still needs some testing!

--- models/01_crud.py
+++ models/01_crud.py
@@ -1317,6 +1317,15 @@ def shn_create(r, **attr):
 
     if representation == "html":
 
+        # Copy from a previous record?
+        original_id = r.request.vars.get("from_record", None)
+        if original_id:
+            copy_fields = [f for f in table if f.readable and f.writable]
+            if shn_has_permission("read", table, original_id):
+                original = db(table.id==original_id).select(*copy_fields, limitby=(0,1)).first()
+            else:
+                original = None
+
         # Default components
         output = dict(module=prefix, resource=name, main=main, extra=extra)
 
@@ -1374,10 +1383,19 @@ def shn_create(r, **attr):
 
         # Get the form
         message = shn_get_crud_string(tablename, "msg_record_created")
-        form = crud.create(table,
-                           message=message,
-                           onvalidation=onvalidation,
-                           onaccept=_onaccept)
+        if original:
+            original.id = None
+            form = crud.update(table,
+                               original,
+                               message=message,
+                               next=crud.settings.create_next,
+                               onvalidation=onvalidation,
+                               onaccept=_onaccept)
+        else:
+            form = crud.create(table,
+                               message=message,
+                               onvalidation=onvalidation,
+                               onaccept=_onaccept)
 
         # Cancel button?
         #form[0].append(TR(TD(), TD(INPUT(_type="reset", _value="Reset form"))))

e026388bfe9a5997d2a1eb02b9cb0fcb05866cea
nursix.org <dominic@nursix.org>
2010-08-27 10:28:44 +0000

    S3ResourceController:   - search_simple searching also in component fields,     specify a component field like "address.city" in     the search fields list

--- modules/s3xrc.py
+++ modules/s3xrc.py
@@ -3616,43 +3616,83 @@ class S3ResourceController(object):
 
         """
 
-        search_fields = []
-        if fields and isinstance(fields, (list,tuple)):
-            for f in fields:
-                if table.has_key(f):
-                    search_fields.append(f)
+        mq = Storage()
+        search_fields = Storage()
+
+        prefix, name = table._tablename.split("_", 1)
+
+        if fields and not isinstance(fields, (list, tuple)):
+            fields = [fields]
+        elif not fields:
+            raise SyntaxError("No search fields specified.")
+
+        for f in fields:
+            _table = None
+            component = None
+
+            if f.find(".") != -1:
+                cname, f = f.split(".", 1)
+                component, pkey, fkey = self.model.get_component(prefix, name, cname)
+                if component:
+                    _table = component.table
+                    tablename = component.tablename
+            else:
+                _table = table
+                tablename = table._tablename
+
+            if _table and tablename not in mq:
+                query = (self.auth.shn_accessible_query("read", _table))
+                if "deleted" in _table.fields:
+                    query = (query & (_table.deleted == "False"))
+                if component:
+                    join = (table[pkey] == _table[fkey])
+                    query = (query & join)
+                mq[_table._tablename] = query
+
+            if _table and f in _table.fields:
+                if _table._tablename not in search_fields:
+                    search_fields[tablename] = [_table[f]]
+                else:
+                    search_fields[tablename].append(_table[f])
+
         if not search_fields:
             return None
 
         if label and isinstance(label,str):
             labels = label.split()
             results = []
-            query = None
+
             for l in labels:
-                # add wildcards
                 wc = "%"
                 _l = "%s%s%s" % (wc, l, wc)
-                for f in search_fields:
-                    if query:
-                        query = (table[f].like(_l)) | query
+                query = None
+                for tablename in search_fields.keys():
+                    hq = mq[tablename]
+                    fq = None
+                    fields = search_fields[tablename]
+                    for f in fields:
+                        if fq:
+                            fq = (f.like(_l)) | fq
+                        else:
+                            fq = (f.like(_l))
+                    q = hq & fq
+                    if query is None:
+                        query = q
                     else:
-                        query = (table[f].like(_l))
-                # undeleted records only
-                query = (table.deleted == False) & (query)
-                # restrict to prior results (AND)
+                        query = query | q
+
                 if results:
                     query = (table.id.belongs(results)) & query
                 if filterby:
                     query = (filterby) & (query)
+
                 records = self.db(query).select(table.id)
-                # rebuild result list
                 results = [r.id for r in records]
-                # any results left?
                 if not results:
                     return None
+
             return results
         else:
-            # no label given or wrong parameter type
             return None
 
 

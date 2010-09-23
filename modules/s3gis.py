9743a2d93e87252e1ed05c40532268271a229e39
Fran Boon <fran@aidiq.com>
2010-09-23 14:13:17 +0100

    Fix KML/geoRSS

--- modules/s3gis.py
+++ modules/s3gis.py
@@ -1290,10 +1290,14 @@ class GIS(object):
 
         # Read configuration
         config = self.get_config()
-        if not height:
-            height = config.map_height
-        if not width:
-            width = config.map_width
+        if height:
+            map_height = height
+        else:
+            map_height = config.map_height
+        if width:
+            map_width = width
+        else:
+            map_width = config.map_width
         if bbox and (-90 < bbox["max_lat"] < 90) and (-90 < bbox["min_lat"] < 90) and (-180 < bbox["max_lon"] < 180) and (-180 < bbox["min_lon"] < 180):
             # We have sane Bounds provided, so we should use them
             pass
@@ -3011,10 +3015,12 @@ OpenLayers.Util.extend( selectPdfControl, {
                         projection_str = "projection: new OpenLayers.Projection('EPSG:" + georss_projection + "'),"
                     marker_id = layer["marker_id"]
                     if marker_id:
-                        marker = db(db.gis_marker.id == marker_id).select(db.gis_marker.image, limitby=(0, 1)).first().image
+                        marker = db(db.gis_marker.id == marker_id).select(db.gis_marker.image, db.gis_marker.height, db.gis_marker.width, limitby=(0, 1)).first()
                     else:
-                        marker = db(db.gis_marker.id == marker_default).select(db.gis_marker.image, limitby=(0, 1)).first().image
-                    marker_url = URL(r=request, c="static", f="img", args=["markers", marker])
+                        marker = db(db.gis_marker.id == marker_default).select(db.gis_marker.image, db.gis_marker.height, db.gis_marker.width, limitby=(0, 1)).first()
+                    marker_url = URL(r=request, c="static", f="img", args=["markers", marker.image])
+                    height = marker.height
+                    width = marker.width
 
                     if cacheable:
                         # Download file
@@ -3237,10 +3243,12 @@ OpenLayers.Util.extend( selectPdfControl, {
                     projection_str = "projection: proj4326,"
                     marker_id = layer["marker_id"]
                     if marker_id:
-                        marker = db(db.gis_marker.id == marker_id).select(db.gis_marker.image, limitby=(0, 1)).first().image
+                        marker = db(db.gis_marker.id == marker_id).select(db.gis_marker.image, db.gis_marker.height, db.gis_marker.width, limitby=(0, 1)).first()
                     else:
-                        marker = db(db.gis_marker.id == marker_default).select(db.gis_marker.image, limitby=(0, 1)).first().image
-                    marker_url = URL(r=request, c="static", f="img", args=["markers", marker])
+                        marker = db(db.gis_marker.id == marker_default).select(db.gis_marker.image, db.gis_marker.height, db.gis_marker.width, limitby=(0, 1)).first()
+                    marker_url = URL(r=request, c="static", f="img", args=["markers", marker.image])
+                    height = marker.height
+                    width = marker.width
                     if cacheable:
                         # Download file
                         file, warning = self.download_kml(url, public_url)
@@ -3595,8 +3603,8 @@ OpenLayers.Util.extend( selectPdfControl, {
 
         mapPanel = new GeoExt.MapPanel({
             region: 'center',
-            height: """ + str(height) + """,
-            width: """ + str(width) + """,
+            height: """ + str(map_height) + """,
+            width: """ + str(map_width) + """,
             id: 'mappanel',
             xtype: 'gx_mappanel',
             map: map,
@@ -3666,8 +3674,8 @@ OpenLayers.Util.extend( selectPdfControl, {
             autoScroll: true,
             maximizable: true,
             titleCollapse: true,
-            height: """ + str(height) + """,
-            width: """ + str(width) + """,
+            height: """ + str(map_height) + """,
+            width: """ + str(map_width) + """,
             layout: 'border',
             items: [{
                     region: 'west',

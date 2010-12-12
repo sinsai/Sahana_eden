353d27d343b7488f8e93c3d2463eed68af26e7ea
Shiv Deepak <idlecool@gmail.com>
2010-12-12 08:52:09 +0530

    fixes 86/780 client side validation

---
+++ views/survey/series_validation.js
@@ -0,0 +1,27 @@
+<!-- form start end date validation for survey/series form -->
+<script type="text/javascript">//<![CDATA[
+jQuery(document).ready(function() {
+    $('.form-container > form').submit(function () {
+	var datePattern = /^(19|20)\d\d([-\/.])(0[1-9]|1[012])\2(0[1-9]|[12][0-9]|3[01])$/;
+	if (!(datePattern.test(this.from_date.value) && datePattern.test(this.to_date.value))) {
+	    error_msg = '{{=T("start date and end date should have valid date value")}}'
+	    jQuery('#survey_series_to_date__row > td').last().text(error_msg);
+	    jQuery('#survey_series_to_date__row > td').last().addClass('red');
+	    return false;
+	}
+	from_date = this.from_date.value.split('-')
+	from_date = new Date(from_date[0], from_date[1], from_date[2])
+	to_date = this.to_date.value.split('-')
+	to_date = new Date(to_date[0], to_date[1], to_date[2])
+	if (from_date > to_date) {
+	    error_msg = '{{=T("start date should be prior to end date")}}'
+	    jQuery('#survey_series_to_date__row > td').last().text(error_msg);
+	    jQuery('#survey_series_to_date__row > td').last().addClass('red');
+	    return false;
+	} else {
+	    return true;
+	}
+    });
+});
+//]]></script>
+

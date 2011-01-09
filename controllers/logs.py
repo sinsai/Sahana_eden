66a216c68c8bab42ba7542bfda6753b5d3e507f1
Fran Boon <fran@aidiq.com>
2011-01-09 14:53:05 +0000

    Fix #909

--- controllers/logs.py
+++ controllers/logs.py
@@ -221,74 +221,67 @@ def shn_logs_send_rheader(r):
                 return rheader
     return None
 #------------------------------------------------------------------------------
-
+# Below unused any more
 #==============================================================================
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
+#def shn_distrib_rheader(r, tabs=[]):
+#    if r.representation == "html":
+#        logs_distrib = r.record
+#        if logs_distrib:
+#            rheader_tabs = shn_rheader_tabs(r, tabs)
+#            rheader = DIV(TABLE(TR(
+#                                   TH(T("Location") + ": "), shn_gis_location_represent(logs_distrib.location_id),
+#                                   TH(T("Date") + ": "), logs_distrib.date,
+#                                   ),
+#                               ),
+#                          rheader_tabs
+#                          )
+#            return rheader
+#    return None
 
-#==============================================================================
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-#==============================================================================
-def shn_distrib_rheader(r, tabs=[]):
-    if r.representation == "html":
-        logs_distrib = r.record
-        if logs_distrib:
-            rheader_tabs = shn_rheader_tabs(r, tabs)
-            rheader = DIV(TABLE(TR(
-                                   TH(T("Location") + ": "), shn_gis_location_represent(logs_distrib.location_id),
-                                   TH(T("Date") + ": "), logs_distrib.date,
-                                   ),
-                               ),
-                          rheader_tabs
-                          )
-            return rheader
-    return None
+#def distrib():
 
-def distrib():
+#    """ RESTful CRUD controller """
 
-    """ RESTful CRUD controller """
-
-    resource = request.function
-    tablename = "%s_%s" % (module, resource)
-    table = db[tablename]
+#    resource = request.function
+#    tablename = "%s_%s" % (module, resource)
+#    table = db[tablename]
 
     # Don't send the locations list to client (pulled by AJAX instead)
-    table.location_id.requires = IS_NULL_OR(IS_ONE_OF_EMPTY(db, "gis_location.id"))
+#    table.location_id.requires = IS_NULL_OR(IS_ONE_OF_EMPTY(db, "gis_location.id"))
 
     # Post-processor
-    def postp(r, output):
-        if r.representation in shn_interactive_view_formats:
+#    def postp(r, output):
+#        if r.representation in shn_interactive_view_formats:
             #if r.method == "create" and not r.component:
             # listadd arrives here as method=None
-            if r.method != "delete" and not r.component:
+#            if r.method != "delete" and not r.component:
                 # Redirect to the Items tabs after creation
-                r.next = r.other(method="distrib_item", record_id=s3xrc.get_session(module, resource))
+#                r.next = r.other(method="distrib_item", record_id=s3xrc.get_session(module, resource))
 
             # Normal Action Buttons
-            shn_action_buttons(r)
+#            shn_action_buttons(r)
 
-        return output
-    response.s3.postp = postp
+#        return output
+#    response.s3.postp = postp
 
-    tabs = [
-            (T("Details"), None),
-            (T("Items"), "distrib_item"),
-           ]
-    rheader = lambda r: shn_distrib_rheader(r, tabs)
+#    tabs = [
+#            (T("Details"), None),
+#            (T("Items"), "distrib_item"),
+#           ]
+#    rheader = lambda r: shn_distrib_rheader(r, tabs)
 
-    output = s3_rest_controller(module, resource, rheader=rheader)
-    return output
+#    output = s3_rest_controller(module, resource, rheader=rheader)
+#    return output
 
 #------------------------------------------------------------------------------
-def distrib_item():
+#def distrib_item():
 
-    """ RESTful CRUD controller """
+#    """ RESTful CRUD controller """
 
-    resource = request.function
-    tablename = "%s_%s" % (module, resource)
-    table = db[tablename]
+#    resource = request.function
+#    tablename = "%s_%s" % (module, resource)
+#    table = db[tablename]
 
-    output = s3_rest_controller(module, resource)
-    return output
+#    output = s3_rest_controller(module, resource)
+#    return output
 #------------------------------------------------------------------------------

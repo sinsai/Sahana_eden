34a5caa9414ca2cc5a3232c31b3aacb7f973ee36
Patricia Tressel <PatriciaTressel@phobos>
2011-01-11 15:28:44 -0800

    Reverting a change made in rev 1350.2.16 that changed an rms_req field name in controllers/cr.py with no corresponding change in the rms_req table. It was a gratuitous field name change anyway, to be avoided because schema name changes lead to by-hand migrations (at the moment).

--- controllers/cr.py
+++ controllers/cr.py
@@ -254,7 +254,7 @@ def shn_shelter_prep(r):
                 if auth.is_logged_in():
                     requestor = db(db.pr_person.uuid == session.auth.user.person_uuid).select(db.pr_person.id, limitby=(0, 1)).first()
                     if requestor:
-                        db.rms_req.requestor_person_id.default = requestor.id
+                        db.rms_req.person_id.default = requestor.id
 
 
             elif r.component.name == "presence":

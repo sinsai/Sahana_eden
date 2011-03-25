9ebb552c8d0a6d3c75799ce3b8ee37a38d1ecd68
fuga <fuga35@gmail.com>
2011-03-25 20:20:35 +0900

    ui.autocomplete does not work on firefox with Japanese input method. This change fix it.

--- static/scripts/S3/jquery.ui.autocomplete.js
+++ static/scripts/S3/jquery.ui.autocomplete.js
@@ -32,7 +32,8 @@ $.widget( "ui.autocomplete", {
 	_create: function() {
 		var self = this,
 			doc = this.element[ 0 ].ownerDocument,
-			suppressKeyPress;
+			suppressKeyPress,
+			keyIsEnter;
 
 		this.element
 			.addClass( "ui-autocomplete-input" )
@@ -49,6 +50,7 @@ $.widget( "ui.autocomplete", {
 				}
 
 				suppressKeyPress = false;
+				keyIsEnter = false;
 				var keyCode = $.ui.keyCode;
 				switch( event.keyCode ) {
 				case keyCode.PAGE_UP:
@@ -76,6 +78,7 @@ $.widget( "ui.autocomplete", {
 						suppressKeyPress = true;
 						event.preventDefault();
 					}
+					keyIsEnter = true;
 					//passthrough - ENTER and TAB both select the current element
 				case keyCode.TAB:
 					if ( !self.menu.active ) {
@@ -105,6 +108,29 @@ $.widget( "ui.autocomplete", {
 					suppressKeyPress = false;
 					event.preventDefault();
 				}
+				if (event.keyCode == $.ui.keyCode.ENTER || event.keyCode ==  $.ui.keyCode.NUMPAD_ENTER) {
+					keyIsEnter = true;
+				}
+			})
+			.bind( "keyup.autocomplete", function( event ) {
+				if (keyIsEnter) {
+					keyIsEnter = false;
+					return;
+				}
+				if (event.keyCode != $.ui.keyCode.ENTER && event.keyCode !=  $.ui.keyCode.NUMPAD_ENTER) {
+					return;
+				}
+				if ( self.options.disabled || self.element.attr( "readonly" ) ) {
+					return;
+				}
+				clearTimeout( self.searching );
+				self.searching = setTimeout(function() {
+					// only search if the value has changed
+					if ( self.term != self.element.val() ) {
+						self.selectedItem = null;
+						self.search( null, event );
+					}
+				}, self.options.delay );
 			})
 			.bind( "focus.autocomplete", function() {
 				if ( self.options.disabled ) {
